4a8f649d1c4fc5e9ea9b3562b84d2c2f30590619
Ben Smith <benlsmith329@gmail.com>
2022-03-29 20:09:38 -0400

    added questions on javascript

---
+++ script.js
@@ -0,0 +1,19 @@
+var questions = [
+    {
+        question: "Which president was in office for the majority of WW2?",
+        choices: ["1. Ronald Reagan", "2. Franklin D. Roosevelt", "3. Harry S. Truman", "4. Dwight D. Eisenhower"],
+        answer: "2. Franklin D. Roosevelt"
+    },
+    {
+        question: "Which president dropped the atomic bomb on August 6, 1945?",
+        choices: ["1. Woodrow Wilson", "2. Franklin D. Roosevelt", "3. Calvin Coolidge", "4. Harry S. Truman"],
+        amswer: "4. Harry S. Truman"
+
+    }
+    {
+        question: "What event lead the US to join WW2",
+        choices: ["1. Nazi Invasion of Poland", "2. Pearl Harbor", "3. Incursions of U-Boats in Atlantic", "4. None of The Above"],
+        answer: "2. Pearl Harbor"
+    }
+
+]
